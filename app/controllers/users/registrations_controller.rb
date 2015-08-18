f296363bdad08926fd9db371abdd08f7ac1e6d68
sunsplat <heeycutie@yahoo.com>
2015-08-18 21:31:30 +0000

    added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     
+    #allows us to run our code before anything in controller runs
+    before_filter :select_plan, only: :new
+    
     def create
         super do |resource| #importing create action code from Devise
             if params[:plan] # sees if there is a 'plan' parameter submitted with form after user presses submit button
@@ -13,5 +16,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end
         end
     end
+    
+    private
+        def select_plan
+            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+            flash[:notice] = "Please select a membership plan to sign up."
+            redirect_to root_url
+            end
+        end
     
 end
