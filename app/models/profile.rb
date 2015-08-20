1c67f579e8d622c60e37f1c3c08a5bdc1f622e97
sunsplat <heeycutie@yahoo.com>
2015-08-20 20:11:21 +0000

    added avatar column to profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,7 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\z/
     validates :first_name, :last_name, presence: true
 end
