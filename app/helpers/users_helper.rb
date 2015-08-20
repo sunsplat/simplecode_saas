a1f001ee8621a18fbe915cd8faec26828cd79e2b
sunsplat <heeycutie@yahoo.com>
2015-08-20 23:24:09 +0000

    add user profile icon and fixed errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
     def job_title_icon
-        if @user.profile.job_title == "House Sitter"
+        if @user.profile.job_title == "House-Sitter"
             "<i class= 'fa fa-heart'></i>".html_safe
-        elsif @user.profile.job_title == "Pet Sitter"
+        elsif @user.profile.job_title == "Pet-Sitter"
             "<i class= 'fa fa-github-alt'></i>".html_safe
-        elsif @user.profile.job_title == "Home Owner"
+        elsif @user.profile.job_title == "Home-Owner"
             "<i class= 'fa fa-h-square'></i>".html_safe
         end
     end
